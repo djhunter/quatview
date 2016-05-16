6f833e1ea4dc1e3625ff61396c220f2210925db9
David J. Hunter <dhunter@westmont.edu>
2016-05-16 10:23:49 -0700

    fixed random s3 points function

--- randomposs3points.R
+++ randomposs3points.R
@@ -1,8 +1,9 @@
 # generates 100 random points on the three sphere (positive?)
-library("pracma", lib.loc="/home/dhunter/R/x86_64-pc-linux-gnu-library/3.1")
-library("orientlib", lib.loc="/home/dhunter/R/x86_64-pc-linux-gnu-library/3.1")
+library("pracma")
+library("orientlib")
 
-ps3pts <- cbind(abs(rnorm(100)),abs(rnorm(100)),abs(rnorm(200)),abs(rnorm(100)))
+# ps3pts <- cbind(abs(rnorm(100)),abs(rnorm(100)),abs(rnorm(100)),abs(rnorm(100)))
+ps3pts <- cbind(rnorm(100),rnorm(100),rnorm(100),rnorm(100))
 ps3pts <- t(apply(ps3pts,1,function(x) x/(sqrt(dot(x,x)))))
 prquats <- quaternion(ps3pts)
 prorients <- as(prquats, 'rotmatrix')
